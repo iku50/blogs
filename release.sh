d8979e586db4638a53ae6f830d7485e1c7278e4c
Rick <wizo.o@outlook.com>
2023-04-11 20:43:03 +0800

    ok

--- release.sh
+++ release.sh
@@ -1,7 +1,13 @@
 hugo
-rm -rf public
+
+if [ $? -ne 0 ]; then
+    echo "Hugo build failed"
+    exit 1
+fi
+if [ -d "public" ]; then
+    rm -rf public
+fi
 mv public docs
 git add .
 git commit -m "ok"
 git push -u origin main
-mv docs public
